d711572c9d6fb0e50b4426fb6860d33ff24c4a17
CofCat456 <a25690756@gmail.com>
2024-06-28 00:07:12 +0800

    feat: implement map zoom、reset

--- packages/core/EMap/src/types.ts
+++ packages/core/EMap/src/types.ts
@@ -5,6 +5,18 @@ export interface EMapProps {
   img: string | HTMLImageElement
 
   /**
+   * The maximum zoom level which will be displayed on the map.
+   */
+  maxZoom?: number
+  /**
+   * The minimum zoom level which will be displayed on the map.
+   */
+  minZoom?: number
+  /**
+   * The initial Map zoom level.
+   */
+  zoom?: number
+  /**
    * The enabled/disabled state of the Zoom control.
    */
   zoomControl?: boolean
--- playground/src/pages/index.vue
+++ playground/src/pages/index.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
 import { EMap } from 'vue-emap'
 
+import type { ComponentExposed } from 'vue-component-type-helpers'
+
+import ControlBtns from '~/components/ControlBtns.vue'
+
 import mapImg from '~/images/map.jpg'
 
 defineOptions({
   name: 'IndexPage',
 })
+
+const EMapRef = shallowRef<ComponentExposed<typeof EMap> | null>(null)
 </script>
 
 <template>
-  <div class="w-[400px] h-[400px]">
-    <EMap :zoom-control="false" :img="mapImg">
+  <div class="w-[800px] h-[800px]">
+    <EMap ref="EMapRef" zoom-control :img="mapImg">
       I Am EMap
     </EMap>
+    <ControlBtns
+      @on-zoom-in="EMapRef?.setZoom(EMapRef.zoomNum + 0.1)"
+      @on-zoom-out="EMapRef?.setZoom(EMapRef.zoomNum - 0.1)"
+      @on-reset="EMapRef?.reset()"
+    />
   </div>
 </template>
