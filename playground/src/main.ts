307f7f80a54993ecfb4b9e16a1fd893e21ae2af3
CofCat456 <a25690756@gmail.com>
2024-08-16 12:19:11 +0800

    fix(playground): fix router

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,20 +1,19 @@
-import type { RouteRecordRaw } from 'vue-router'
-
 import '@airataiwan/editor/dist/index.css'
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createApp } from 'vue'
 import 'vue-emap/dist/index.css'
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 
 import App from './App.vue'
 import './styles/main.css'
 
 const app = createApp(App)
 const router = createRouter({
-  extendRoutes: (routes: RouteRecordRaw[]) => setupLayouts(routes),
   history: createWebHistory(import.meta.env.BASE_URL),
+  routes: setupLayouts(routes),
 })
 app.use(router)
 app.mount('#app')
